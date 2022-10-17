8e157ea87bd12eb2da4fcbc31e65e7c00a73d40d
oskar <pinkfloyd951@naver.com>
2022-10-17 18:16:28 +0900

    [RND-1355][LIB] Build폴더에 폰트 파일 추가

--- dist/common/style.js
+++ dist/common/style.js
@@ -3,7 +3,7 @@ var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cook
     return cooked;
 };
 import { createGlobalStyle } from "styled-components";
-import NotoSansMedium from "src/common/fonts/NotoSansKR-Medium.otf";
+import NotoSansMedium from "./fonts/NotoSansKR-Medium.otf";
 var FontStyles = createGlobalStyle(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n\n@font-face {\n  font-family: 'Noto sans';\n  src: url(", ");\n}\n"], ["\n\n@font-face {\n  font-family: 'Noto sans';\n  src: url(", ");\n}\n"])), NotoSansMedium);
 export default FontStyles;
 var templateObject_1;
--- src/common/style.ts
+++ src/common/style.ts
@@ -1,11 +1,11 @@
 import { createGlobalStyle } from "styled-components";
-import NotoSansMedium from "src/common/fonts/NotoSansKR-Medium.otf";
+import NotoSansMedium from "./fonts/NotoSansKR-Medium.otf";
 
 const FontStyles = createGlobalStyle`
 
 @font-face {
   font-family: 'Noto sans';
-  src: url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500&display=swap');
+  src: url(${NotoSansMedium});
 }
 `;
 
